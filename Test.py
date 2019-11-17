4af1e0b29e527cfd9d00f7126d9ee7e55fa809c7
Jane616 <55512796+Jane616@users.noreply.github.com>
2019-11-17 17:32:51 +0000

    update access token setting

--- Test.py
+++ Test.py
@@ -1,8 +1,12 @@
 from github import Github
 
-ACCESS_TOKEN = '7d1c0857203613ccc1256c6efeea47bd8b7d809f'
-g = Github(ACCESS_TOKEN)
+#initialze github access
+#access_token = '9b0780e14a2d6ac8aab4aca1096ed3a6855fb8ba'
+print('please input personal access token: ')
+access_token = input()
+g = Github(access_token)
 
+#get remaining rate limit
 rate_limit = g.get_rate_limit()
 rate = rate_limit.search
 if rate.remaining == 0:
@@ -10,13 +14,13 @@ if rate.remaining == 0:
 else:
     print(f'You have {rate.remaining}/{rate.limit} API calls remaining')
 
+#construct and perform a query
 keywords = 'tetris'
 query = keywords + '+language:assembly'
 result = g.search_repositories(query,'stars', 'desc')
-#result = g.search_repositories(query='language:python')
 
+#access information returned by query
 print(f'Found {result.totalCount} repo(s)')
- 
 for repo in result:
     print(f'login: {repo.owner.login}')
     print(f'repo name: {repo.name}')
