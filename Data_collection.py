6d957d3d9589336205e6f5baab0847c15f14d3fd
Jane616 <55512796+Jane616@users.noreply.github.com>
2019-11-17 17:09:21 +0000

    add rate limit solution & company_query function

--- Data_collection.py
+++ Data_collection.py
@@ -1,19 +1,40 @@
 from github import Github
+from datetime import datetime
+from datetime import time
+
+#pause program if about to exceed rate limit
+def api_wait_search(git):
+  limits = git.get_rate_limit()
+  print(f'remaining limit: {limits.search.remaining}')
+  if limits.search.remaining <= 2:
+    seconds = (limits.search.reset - datetime.now()).total_seconds()
+    print ("Waiting for %d seconds ..." % (seconds))
+    time.sleep(seconds)
+    print ("Done waiting - resume!")
+
+
+#get list of company employees
+def company_query(g, keyword):
+    query = f'"{keyword}" in:org type:user repos:30..50'
+    result = g.search_users(query)
+    return result
+
+
 
 #initialze github access
-ACCESS_TOKEN = 'b6ef885e0820f6f9bd58b2a89a163bf3b57877c8'
-g = Github(ACCESS_TOKEN)
+#c_id = '2b9f6e2c97d4a2dc6c67'
+#c_secret = '2b9f6e2c97d4a2dc6c67'
+access_token = '8e3db69a6c3c3999b38722dfc0419c784adb8f5b'
+g = Github(access_token)
 
+
+api_wait_search(g)
 #search microsoft employees
 keyword = 'microsoft'
-query = f'"{keyword}" in:org type:user repos:30..50'
-result = g.search_users(query)
+result = company_query(g, keyword)
+print(f'Found {result.totalCount} {keyword} employees')
+api_wait_search(g)
 
-#restricting number of api calls
-max_size = 10
-print(f'Found {result.totalCount} user(s)')
-if result.totalCount > max_size:
-    result = result[:max_size]
 
 #print info
 for user in result:
